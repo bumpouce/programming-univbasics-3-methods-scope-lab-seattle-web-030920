6b184ab6d399a74d5f8668a03579230327d68cba
Christine Bumpous <bumpouce@gmail.com>
2020-01-17 02:33:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,11 @@ def mario
 end
 
 def toadstool
-    status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-    puts status
+   status = "Thank You Mario! But Our Princess Is In Another Castle!"
+   puts status
+end
+
+def link
+  saying = "It's Dangerous To Go Alone! Take This."
+  puts saying
 end
